a38db357b619f7dd7cb2410560454364edd276f3
Scottt <scott.geyer@eagles.usm.edu>
2017-03-03 17:59:15 -0600

    Update Mario-Lozano.sh

--- Mario-Lozano.sh
+++ Mario-Lozano.sh
@@ -4,5 +4,4 @@ xset s off # disable screen saver
 xset s noblank # don't blank the video device
 matchbox-window-manager & qjoypad "FOIL-Mario" &
 midori -e Fullscreen -a https://phosphorus.github.io/app.html?id=148302406&turbo=false&full-screen=true
-# This uses Phosophorus and Midori to run Geometry Jump by Punkbass 
-# (Based on Unit 8 from BootStrap World: http://www.bootstrapworld.org/materials/spring2017/courses/bs1/units/unit8/index.html )
+# This uses Phosophorus and Midori to run Super Mario Lozano by Punkbass and QJoyPad to configure a controller
