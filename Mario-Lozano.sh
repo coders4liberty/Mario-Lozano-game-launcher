457b5eca7cb48559fb30760554221d04bcaa4df1
Scottt <scott.geyer@eagles.usm.edu>
2017-03-04 16:52:36 -0600

    Update Mario-Lozano.sh

--- Mario-Lozano.sh
+++ Mario-Lozano.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-xset -dpms # disable DPMS (Energy Star) features.
-xset s off # disable screen saver
-xset s noblank # don't blank the video device
-matchbox-window-manager & qjoypad "FOIL-Mario" &
-midori -e Fullscreen -a https://phosphorus.github.io/app.html?id=148302406&turbo=false&full-screen=true
-# This uses Phosophorus and Midori to run Super Mario Lozano by Punkbass and QJoyPad to configure a controller
+qjoypad "FOIL-Mario" &
+chromium-browser --app=https://phosphorus.github.io/app.html?id=148302406&turbo=false&full-screen=true --start-fullscreen
+# This uses Phosophorus and Chromium to run Super Mario Lozano by Punkbass and QJoyPad to configure a controller
